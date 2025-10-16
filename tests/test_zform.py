c711e6895271e76bff8a6edb3055d6f8763aaa10
NicoloZorzetto <nicolozorzetto02@gmail.com>
2025-10-17 00:42:18 +0200

    pandas import error

--- tests/test_zform.py
+++ tests/test_zform.py
@@ -32,12 +32,23 @@ def try_zform_penguins():
             "You do not have seaborn installed"
         )
     while True:
-        which_data = input("Would you like to run the small (input 1) or big (input 2) dataset? ")
+        print("Which dataset do you want to use?")
+        print("\t 1 - small penguins dataset (original 333 rows)")
+        print("\t 2 - big penguins dataset (scaled to 333'000 rows)")
+        which_data = input()
         if which_data == "1":
             df = sbn.load_dataset("penguins").dropna()
             break
         if which_data == "2":
             df = sbn.load_dataset("penguins").dropna()
+            try:
+                import pandas as pd
+            except ImportError as e:
+                raise ImprtError(
+                    "You do not have pandas installed."
+                       "Please install all requirements via "
+                      "'pip install-r requirements.txt' "
+                )
             df = pd.concat([df] * 1000, ignore_index=True)
             break
         else:
@@ -46,12 +57,35 @@ def try_zform_penguins():
         df,
         group_col="species",
         return_results=True,
-	    min_obs = 30,
+	    min_obs = 10,
 	    apply = True,
 	    naming = "standard",
         export_csv="./forms.csv",
         n_jobs=-1
     )
+
+    while True:
+        print_question = input("Would you like to check the results? (y/n) ")
+        if print_question.lower()[0] == "y":
+            print("Which results would you like to check?")
+            print("\t 1 - forms dataframe")
+            print("\t 2 - forms best metric")
+            print("\t 3 - original and transformed columns after application to original df (standard naming)")
+            which_print = input("")
+            if which_print[0] == '1':
+                print(forms.head(10))
+                print("To view different naming options use help(zform) or help(zform_apply)")
+            elif which_print[0] == '2':
+                print(list(forms.columns)[4])
+                print(forms[list(forms.columns)[4]].head(10))
+            elif which_print[0] == '3':
+                print(df_out.head(10))
+            else:
+                print("Please input a valid selection.")
+        elif print_question.lower()[0] == "n":
+            break
+        else:
+            print("Please input just y or n.")
 
 
 if __name__ == "__main__":
